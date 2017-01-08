810551d5543c4d2e93175b3da0be8a3a49f92aaa
PavelGora <spec4web@gmail.com>
2017-01-08 22:41:05 +0300

    changed Format fields to public

--- src/com/company/Format.java
+++ src/com/company/Format.java
@@ -4,8 +4,8 @@ package com.company;
  * Created by asus on 08.01.2017.
  */
 public class Format {
-    private static final String INFO = (char) 27 + "[37m";
-    private static final String SUCCESS = (char) 27 + "[32m";
-    private static final String FAIL = (char) 27 + "[31m";
-    private static final String END = (char) 27 + "[0m";
+    public static final String INFO = (char) 27 + "[37m";
+    public static final String SUCCESS = (char) 27 + "[32m";
+    public static final String FAIL = (char) 27 + "[31m";
+    public static final String END = (char) 27 + "[0m";
 }
